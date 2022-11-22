fb1bbaf01fcf5b9c2cbff6c96b1189c4e5ba7b6f
Kaito Matsumoto <spr2140079@stu.o-hara.ac.jp>
2022-11-22 11:15:20 +0900

    グラフ(仮)

--- app/src/main/java/com/example/studytest2/BouGraphActivity.kt
+++ app/src/main/java/com/example/studytest2/BouGraphActivity.kt
@@ -1,23 +1,39 @@
 package com.example.studytest2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.studytest2.databinding.ActivityBouBinding
-import com.example.studytest2.databinding.FragmentGraphBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 
 class BouGraphActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityBouBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bou)
 
-        binding = ActivityBouBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val entries = ArrayList<BarEntry>()
+        entries.add(BarEntry(1f,1f))
+        entries.add(BarEntry(2f,2f))
+        entries.add(BarEntry(3f,3f))
+        entries.add(BarEntry(4f,4f))
+        entries.add(BarEntry(5f,4f))
 
-        binding
 
 
-        }
+        //表示するデータをDataSetに追加
+        val dataSet = BarDataSet(entries, "Label")
+
+        //DataSetをDataに追加
+        val data = BarData(dataSet)
+
+        //ビューを取得
+        val chart = findViewById<BarChart>(R.id.bar_chart)
+
+        //ChartにDataを追加
+        chart.data = data
+
+        //チャートを更新
+        chart.invalidate()
     }
-
+}
--- app/src/main/java/com/example/studytest2/EnGraphActivity.kt
+++ app/src/main/java/com/example/studytest2/EnGraphActivity.kt
@@ -1,11 +1,60 @@
 package com.example.studytest2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+
 
 class EnGraphActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_en_graph)
+
+        val pieChart = findViewById<PieChart>(R.id.piechart)
+
+        pieChart.setUsePercentValues(true)
+        pieChart.getDescription().setEnabled(false)
+        pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f)
+
+        pieChart.setDrawHoleEnabled(false)
+        pieChart.setHoleColor(Color.BLACK)
+        pieChart.setTransparentCircleRadius(61f)
+
+        val yValues = ArrayList<PieEntry>()
+
+        yValues.add(PieEntry(54.73f, "道徳"))
+        yValues.add(PieEntry(12.77f, "倫理"))
+        yValues.add(PieEntry(13.08f, "理科"))
+        yValues.add(PieEntry(12.36f, "数学"))
+        yValues.add(PieEntry(7.06f, "英語"))
+
+
+        val description = Description()
+        description.setText("過去一年間やった教科の割合")
+
+        description.setTextSize(20f)
+        pieChart.setDescription(description)
+
+
+
+        val dataSet = PieDataSet(yValues, "教科名")
+        dataSet.sliceSpace = 3f
+        dataSet.selectionShift = 5f
+        dataSet.setColors(*ColorTemplate.JOYFUL_COLORS)
+
+        val data = PieData(dataSet)
+        data.setValueTextSize(10f)
+        data.setValueTextColor(Color.BLACK)
+
+        pieChart.setData(data)
     }
+
 }
